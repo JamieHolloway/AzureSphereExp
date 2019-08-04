58ca13c947bcd7b57ff8a0efec6f464e99e8b617
Jamie Holloway <jamie@jamieholloway.com>
2019-08-04 09:08:03 -0700

    learning how to code in c again

--- main.c
+++ main.c
@@ -6,22 +6,20 @@
 #include <applibs/log.h>
 #include <applibs/gpio.h>
 #include "mt3620_rdb.h"
+#include "helpers.h"
 
-static const GPIO_Id ledsPins[3][3] = { {MT3620_RDB_LED1_RED, MT3620_RDB_LED1_GREEN, MT3620_RDB_LED1_BLUE}, {MT3620_RDB_LED2_RED, MT3620_RDB_LED2_GREEN, MT3620_RDB_LED2_BLUE}, {MT3620_RDB_LED3_RED, MT3620_RDB_LED3_GREEN, MT3620_RDB_LED3_BLUE} };
+int led1GreenFd = 0;
+int led1BlueFd = 0;
+const struct timespec sleepTime = { 2, 0 };
 
-static int led1Fd = -1;
+static int Init(void);
 
 int main(void)
 {
 	Log_Debug("Experiments with Azure Sphere\n");
 
-	int led1GreenFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
-	if (led1GreenFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
+	if (Init() == -1) return -1;
 
-	int led1BlueFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_BLUE, GPIO_OutputMode_PushPull, GPIO_Value_High);
-	if (led1BlueFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
-
-	const struct timespec sleepTime = { 2, 0 };
 	for (;;)
 	{
 		GPIO_SetValue(led1BlueFd, GPIO_Value_High);
@@ -31,4 +29,18 @@ int main(void)
 		GPIO_SetValue(led1GreenFd, GPIO_Value_High);
 		nanosleep(&sleepTime, NULL);
 	}
+	return 1;
+}
+
+static int Init(void)
+{
+	Log_Debug("Init and setup\n");
+
+	DebugPrintCurrentlyConnectedWiFiNetwork();
+
+	led1GreenFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (led1GreenFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
+
+	led1BlueFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_BLUE, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (led1BlueFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
 }
