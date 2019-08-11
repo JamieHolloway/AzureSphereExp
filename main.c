bcaf4f28706828ee62e9555ba19d9b6ee369c9be
Jamie Holloway <jamie@jamieholloway.com>
2019-08-10 20:38:50 -0700

    working with events

--- main.c
+++ main.c
@@ -9,15 +9,18 @@
 #include "epoll_timerfd_utilities.h"
 #include "helpers.h"
 
+bool programRun = true; // set to false to terminate program
 static int epollFd = -1;
+
+struct timespec buttonPressCheckPeriod = { 0, 1000000 };
 void ButtonPollTimerEventHandler(EventData* eventData);
 static EventData buttonPollEventData = { .eventHandler = &ButtonPollTimerEventHandler };
 static int buttonGpioFd = -1;
 static int buttonPollTimerFd = -1;
 GPIO_Value_Type buttonState = GPIO_Value_High;
+
 static int led1GreenFd = -1;
-static int led1BlueFd = -1;
-const struct timespec sleepTime = { 2, 0 };
+static int led1RedFd = -1;
 
 static int Init(void);
 static int CloseDown(void);
@@ -28,18 +31,11 @@ int main(void)
 
 	if (Init() == -1) return -1;
 
-	for (;;)
+	while(programRun)
 	{
-		GPIO_SetValue(led1BlueFd, GPIO_Value_High);
-		GPIO_SetValue(led1GreenFd, GPIO_Value_Low);
-		nanosleep(&sleepTime, NULL);
-		GPIO_SetValue(led1BlueFd, GPIO_Value_Low);
-		GPIO_SetValue(led1GreenFd, GPIO_Value_High);
-		nanosleep(&sleepTime, NULL);
+		GPIO_SetValue(led1GreenFd, GPIO_Value_Low); // led1 green is program running
 
-		if (WaitForEventAndCallHandler(epollFd) != 0) Log_Debug("timer event\n");
-
-		if (IsButtonPressed(buttonGpioFd, &buttonState)) { Log_Debug("button pressed\n"); }
+		if (WaitForEventAndCallHandler(epollFd) != 0) { programRun = false; }
 	}
 
 	CloseDown();
@@ -58,29 +54,39 @@ static int Init(void)
 
 	if (!OpenGpioFdAsInput(MT3620_RDB_BUTTON_A, &buttonGpioFd)) { Log_Debug("Error opening button A.\n"); return -1; }
 
-	struct timespec buttonPressCheckPeriod = { 0, 1000000 };
 	buttonPollTimerFd = CreateTimerFdAndAddToEpoll(epollFd, &buttonPressCheckPeriod, &buttonPollEventData, EPOLLIN);
 	if (buttonPollTimerFd < 0) { Log_Debug("Error creating buttonPollTimerFd.\n"); return -1; }
 
 	led1GreenFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_GREEN, GPIO_OutputMode_PushPull, GPIO_Value_High);
 	if (led1GreenFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
 
-	led1BlueFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_BLUE, GPIO_OutputMode_PushPull, GPIO_Value_High);
-	if (led1BlueFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
+	led1RedFd = GPIO_OpenAsOutput(MT3620_RDB_LED1_RED, GPIO_OutputMode_PushPull, GPIO_Value_High);
+	if (led1RedFd < 0) { Log_Debug("Error opening GPIO: %s (%d).\n", strerror(errno), errno); return -1; }
 
 	return 0;
 }
 
 static int CloseDown()
 {
+	Log_Debug("exiting program in 10 seconds\n");
+	GPIO_SetValue(led1GreenFd, GPIO_Value_High);
+	GPIO_SetValue(led1RedFd, GPIO_Value_Low);
+	const struct timespec sleepTime = { 10, 0 };
+	nanosleep(&sleepTime, NULL);
+	GPIO_SetValue(led1RedFd, GPIO_Value_High);
+
 	if (buttonPollTimerFd >= 0) close(buttonPollTimerFd);
 	if (led1GreenFd >= 0) close(led1GreenFd);
-	if (led1BlueFd >= 0) close(led1BlueFd);
+	if (led1RedFd >= 0) close(led1RedFd);
 	if (epollFd >= 0) close(epollFd);
 }
 
 void ButtonPollTimerEventHandler(EventData* eventData)
 {
 	if (ConsumeTimerFdEvent(buttonPollTimerFd) != 0) { Log_Debug("Error in ButtonPollTimerEventHandler.\n"); return; }
-	Log_Debug("ButtonPollTimeEventHandler reached.\n");
+	if (IsButtonPressed(buttonGpioFd, &buttonState)) 
+	{ 
+		Log_Debug("button A pressed -- shut down program\n"); 
+		programRun = false;
+	}
 }
